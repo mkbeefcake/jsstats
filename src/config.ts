2f601c8d75fe5f5ebee3419a32eb3b6a0dcd0f5a
Joystream Stats <dev@joystreamstats.live>
2023-01-27 10:20:12 +0000

    config update

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,19 @@
 export const historyDepth = 336;
-export const domain = "https://pioneer.joystreamstats.live";
-export const wsLocation = "wss://pl.joystreamstats.live/rpc";
+
+export const domain = "https://pioneerapp.xyz";
+//export const domain = "https://pioneer.joystreamstats.live";
+
+export const wsLocation = "wss://rpc.joystream.org";
+//export const wsLocation = "wss://pl.joystreamstats.live/rpc";
+
+export const hydraLocation = "https://dp.0x2bc.com/graphql"
+export const queryNode= "https://tiguan08.com/graphql"
+
 export const apiLocation = "https://joystreamstats.live/api"
 export const socketLocation = "/socket.io"
-export const hydraLocation = "https://pl.joystreamstats.live/graphql"
-export const queryNode= "https://pl.joystreamstats.live/graphql"
+
 //export const alternativeBackendApis = "http://localhost:3000"
 export const alternativeBackendApis = "https://validators.joystreamstats.live"
+
 export const tasksEndpoint = "https://api.joystreamstats.live/tasks"
 // export const tasksEndpoint = "http://localhost:3500/tasks"
